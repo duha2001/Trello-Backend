42e73d68c9a9ab6b48e7deaada19f2a96ac4b3e1
DESKTOP-AUNIITC\anhdu <duha2001@github.com>
2023-10-02 16:18:45 +0700

    add database

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,3 +3,7 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+// anhdudev
+// KYZySV40GqGAVfdk
+const MONGODB_URI =
+  'mongodb+srv://anhdu2001:KYZySV40GqGAVfdk@cluster0-anhdu.ax76dmf.mongodb.net/?retryWrites=true&w=majority';
