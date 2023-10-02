15a0e53ab1e086accc22c07d683eb798f44b33df
DESKTOP-AUNIITC\anhdu <duha2001@github.com>
2023-10-02 11:22:12 +0700

    Init Project

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const hostname = 'localhost';
+const port = 3017;
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Đã kết nối http://${hostname}:${port}/`);
+});
