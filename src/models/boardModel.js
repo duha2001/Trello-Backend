68fe6feaf094196b4a9caf288d1954efaefeec14
DESKTOP-AUNIITC\anhdu <duha2001@github.com>
2023-10-17 12:54:13 +0700

    Create api board & validate data backend

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,4 +1,7 @@
 import Joi from 'joi';
+import { ObjectId } from 'mongodb';
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators';
+import { GET_DB } from '~/config/mongodb';
 
 // Define Collection (Name & Scheme)
 const BOARD_COLLECTION_NAME = 'boards';
@@ -7,14 +10,50 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   slug: Joi.string().required().min(3).trim().strict(),
   description: Joi.string().required().min(3).max(256).trim().strict(),
 
-  columnOrderIds: Joi.array().items(Joi.string()).default([]),
+  columnOrderIds: Joi.array()
+    .items(Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE))
+    .default([]),
   createAt: Joi.date().timestamp('javascript').default(Date.now),
   updateAt: Joi.date().timestamp('javascript').default(null),
   // Destroy có nghĩa là bản ghi đã bị xóa hay chưa
-  _destroy: Joi.boolean().default(false)
+  _destroy: Joi.boolean().default(false),
 });
+
+// Validation Data before Create
+const validateBeforeCreate = async (data) => {
+  return await BOARD_COLLECTION_SCHEMA.validateAsync(data, {
+    abortEarly: false,
+  });
+};
+
+const createNew = async (data) => {
+  try {
+    const validData = await validateBeforeCreate(data);
+    const createdBoard = await GET_DB()
+      .collection(BOARD_COLLECTION_NAME)
+      .insertOne(validData);
+    return createdBoard;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const findOneById = async (id) => {
+  try {
+    const result = await GET_DB()
+      .collection(BOARD_COLLECTION_NAME)
+      .findOne({
+        _id: new ObjectId(id),
+      });
+    return result;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
 export const boardModel = {
   BOARD_COLLECTION_NAME,
-  BOARD_COLLECTION_SCHEMA
-}
+  BOARD_COLLECTION_SCHEMA,
+  createNew,
+  findOneById,
+};
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-useless-catch */
 import ApiError from '~/utils/ApiError';
 import { slugify } from '~/utils/formatters';
+import { boardModel } from '~/models/boardModel';
 
 // Cần truyền vào một reqBody để hứng dữ liệu từ tầng Controller
 const createNew = async (reqBody) => {
@@ -12,11 +13,16 @@ const createNew = async (reqBody) => {
     };
 
     // Gọi tới tầng Model để xử lý lưu bản ghi newBoard vào trong Database
-    // ...
+    const createdBoard = await boardModel.createNew(newBoard);
+    console.log(createdBoard);
+
+    // Lấy bảng ghi board sau khi gọi
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId);
+    console.log(getNewBoard);
 
     // Làm thêm các xử lý logic khác với các Collection khác tùy vào dự án,...
     // Bắn email, notification về cho admin khi có 1 cái board mới được tạo,...
-    return newBoard;
+    return getNewBoard;
   } catch (error) {
     throw error;
   }
